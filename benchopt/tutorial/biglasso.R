029eff80bd901934cf254ba3ba08ea00bd2b257e
GD <gd.dev@libertymail.net>
2021-01-22 11:32:08 +0100

    enable multi-threading

--- benchopt/tutorial/biglasso.R
+++ benchopt/tutorial/biglasso.R
@@ -3,12 +3,14 @@
 # Main algorithm
 biglasso_imp <- function(X, Y, lambda_min, n_iter) {
     require(biglasso)
+    require(parallel)
     
     X_bm <- as.big.matrix(X)
     
     out <- biglasso(
         X_bm, Y, penalty="lasso", lambda.min = lambda_min, 
-        family="gaussian", eps=1e-14, ncores=1, max.iter = n_iter
+        family="gaussian", eps=1e-14, ncores=detectCores(), 
+        max.iter = n_iter
     )
     
     return(list(beta=as.matrix(out$beta[-1,])))
