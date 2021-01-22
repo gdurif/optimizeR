b9c2a54d3d19f5bed1fce33b0ffdad41b75a9bee
GD <gd.dev@libertymail.net>
2021-01-22 12:05:55 +0100

    sources for spams tutorial

---
+++ spams/tutorial/tuto_spams.R
@@ -0,0 +1,70 @@
+## Dictionary learning
+
+
+
+#------------------------------------------------------------------------------#
+## Matrix factorization
+
+# image
+library(jpeg)
+img = readJPEG(file.path("spams", "tutorial", "data", "image.jpg")) # edit path
+str(img)
+
+# channel extraction
+img <- img[,,1]
+str(img)
+
+# image orientation and visualization
+rotate <- function(x) t(apply(x, 2, rev))
+img = rotate(img)
+image(img)
+
+# sparse NMF in SPAMS
+library(spams)
+res <- spams.nmf(img, return_lasso = TRUE, K = 5, numThreads = 4, iter = -5)
+str(res)
+U = as.matrix(res[[1]])
+V = as.matrix(res[[2]])
+
+# compare X size and sizes of U and V
+prod(dim(img))
+prod(dim(U)) + prod(dim(V))
+
+# image recontruction
+img_rec <- U %*% V
+image(img_rec)
+
+# image reconstruction for different values of K
+par(mfrow=c(2,2))
+
+K_val = c(5, 10, 20, 50)
+res <- Reduce("rbind", lapply(
+    K_val,
+    function(K) {
+        res <- spams.nmf(img, return_lasso = TRUE, K = K, numThreads = 4, iter = -5)
+        U = as.matrix(res[[1]])
+        V = as.matrix(res[[2]])
+        
+        # compare X size and sizes of U and V
+        print(paste0("K = ", K))
+        print(paste0("initial image memory footprint = ", prod(dim(img))))
+        print(paste0(
+            "reconstructed image memory footprint = ", prod(dim(U)) + prod(dim(V))
+        ))
+        print(paste0("ratio = ", (prod(dim(U)) + prod(dim(V)))/prod(dim(img))))
+        
+        # image recontruction
+        img_rec <- U %*% V
+        image(img_rec)
+        
+        # output
+        out = data.frame(
+            K = K,
+            init_memory_footprint = prod(dim(img)),
+            reconstruct_memory_footprint = prod(dim(U)) + prod(dim(V)),
+            ratio = (prod(dim(U)) + prod(dim(V)))/prod(dim(img))
+        )
+    }
+))
+
+res
